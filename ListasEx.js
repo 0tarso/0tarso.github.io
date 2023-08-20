882013422638d47c77246f16e924957eee3e7f6f
0tarso <131756490+0tarso@users.noreply.github.com>
2023-08-20 02:59:44 -0300

    Add files via upload

---
+++ ListasEx.js
@@ -0,0 +1,46 @@
+const contentFirst = document.getElementById('firstDec')
+const contentSecond = document.getElementById('secondDec')
+const firstBtn = document.getElementById('firstBtn')
+const secondBtn = document.getElementById('secondBtn')
+
+firstBtn.addEventListener('click', function(){
+  firstBtn.classList.toggle('aberto') //troca os temas dos botões
+  if(contentFirst.style.display === 'block'){  //se estiver com display block
+    contentFirst.style.display = 'none'        //trocará para o none
+    contentFirst.style.opacity = '0'           //e colocara a opacidade de volta a zero 
+  }
+  else {
+    setTimeout(function(){
+      contentFirst.style.opacity = '1'
+      }, 100) 
+    contentFirst.style.display = 'block'
+    let offsetTop = contentFirst.offsetTop
+  window.scrollTo({
+    top: offsetTop,
+    behavior: "smooth"
+  }) 
+    
+  }
+  
+})
+
+secondBtn.addEventListener('click', function(){
+  secondBtn.classList.toggle('aberto')
+  if(contentSecond.style.display === 'block'){
+     contentSecond.style.display = 'none'
+     contentSecond.style.opacity = '0'
+  }else {
+    setTimeout(function(){
+      contentSecond.style.opacity = '1'
+    }, 100)
+
+    
+    contentSecond.style.display = 'block'
+    let offsetTop = contentSecond.offsetTop
+  window.scrollTo({
+    top:offsetTop,
+    behavior:"smooth"
+  }) 
+
+  }
+})
